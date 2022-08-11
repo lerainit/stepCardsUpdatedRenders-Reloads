c43caaa154fe953def41d8a558b55f8b173c2b40
Valeria Vitvytska <leranmu@gmail.com>
2022-08-11 23:26:18 +0300

    Update cardforTherapist.js

--- classes/cardforTherapist.js
+++ classes/cardforTherapist.js
@@ -1,5 +1,5 @@
 import Card from "./card.js";
-import renderAfterReloadandDelete from "../functions/renderAfterReloadAndDelete.js";
+
 import instance from "../functions/instance.js";
 
 class CardforTherapist extends Card{
@@ -18,20 +18,7 @@ this.ul.insertAdjacentHTML('beforeend',`
 <li><span>Дата последнего визита:</span>${this._age}</li>
 
 `)
-this.deleteBtn.addEventListener('click',()=>{
-    console.log(this._id)
 
-   const deleteCard = async ()=>{
-        const result = await instance.delete(`/${this._id}`)
-        console.log(result)
-    
-    }
-    deleteCard()
-    localStorage.setItem('deletedcard',this._id)
-  
-  renderAfterReloadandDelete()
-this.cardContainer.remove()
-})
 
 
 
